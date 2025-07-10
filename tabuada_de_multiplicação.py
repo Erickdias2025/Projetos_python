4b613bcb8e3a3c84c49e1e3d58dc0344b34f5ed8
Erick Dias <erickdias20255@gmail.com>
2025-07-10 20:08:30 -0300

    MELHORIA DE COMENTÁRIOS

--- tabuada_de_multiplicação.py
+++ tabuada_de_multiplicação.py
@@ -1,17 +1,22 @@
 # TITULO
+
 print("TABUADA DE MULTIPLICAÇÃO")
 
 # ESCOLHA DE VALOR
+
 while True:
     try:
         escolha = int(input("ESCOLHA UM VALOR INTEIRO: "))
+
 # LOOP DE ERRO
+
     except ValueError:
         print(
             "\033[31mENTRADA INVÁLIDA!, TENTE NOVAMENTE USANDO NÚMEROS INTEIROS EX: 1, 10, 100\033[m")
         continue
 
 # RESULTADO DA ESCOLHA DO VALOR DA TABUADA
+
     for i in range(1, 11):
         print(f"{escolha:2} X {i:2} = {i*escolha:3}")
     break
