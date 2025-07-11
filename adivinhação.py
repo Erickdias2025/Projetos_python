99f1739730739e04ff91e18008b4ef54d6de55de
Erick Dias <erickdias20255@gmail.com>
2025-07-11 19:34:38 -0300

    FOI ADICIONADO UM LEITOR DE QUANTIDADE DE TENTATIVAS FEITAS

--- adivinhação.py
+++ adivinhação.py
@@ -15,7 +15,7 @@ while True:
         print(
             "\033[31mENTRADA INVÁLIDA!, TENTE NOVAMENTE USANDO APENAS NÚMEROS INTEIROS EX: 1, 10, 100\033[m")
         continue
-
+    tentativas.append(jogador)
     # SEPARANDO AS OPÇÕES DE ADVINHAÇÕES
     if jogador < escolha:
         print("\033[31mQUASE LÁ!, TENTE UM NÚMERO MAIOR!\033[m")
@@ -24,5 +24,6 @@ while True:
         print("\033[31mQUASE LÁ!, TENTE UM NÚMERO MENOR!\033[m")
 
     else:
-        print("\033[32mPARÁBENS! VOCÊ CONSEGUIU!\033[m")
+        print(
+            f"\033[32mPARÁBENS! VOCÊ CONSEGUIU! FORAM NECESSÁRIAS {len(tentativas)} TENTATIVAS!\033[m")
         break
