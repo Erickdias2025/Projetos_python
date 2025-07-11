25fa472530a6c4da462ae1305289a6d4eaa49c83
Erick Dias <erickdias20255@gmail.com>
2025-07-11 19:33:52 -0300

    TABUADA DE MULTIPLICAÇÃO, FOI ATRIBUIDO A OPÇÃO DE ESCOLHER SUA OPERAÇÃO MATEMÁTICA

--- tabuada_de_multiplicação.py
+++ tabuada_de_multiplicação.py
@@ -1,24 +1,46 @@
 # TITULO
 
-print("TABUADA DE MULTIPLICAÇÃO")
+print("TABUADA")
 
-# ESCOLHA DE VALOR
+multiplicação = "3"
+# ESCOLHA DE OPERAÇÃO MATEMÁTICA
+print("OPERAÇÃO MATEMÁTICA")
+print("1 = +")
+print("2 = -")
+print("3 = *")
+print("4 = /")
 
 while True:
     try:
-        escolha = int(input("ESCOLHA UM VALOR INTEIRO: "))
+        escolha_sinal = int(input("OPERAÇÃO MATEMÁTICA: "))
+        escolha_valor = int(input("VALOR INTEIRO: "))
 
 # LOOP DE ERRO
-
     except ValueError:
         print(
             "\033[31mENTRADA INVÁLIDA!, TENTE NOVAMENTE USANDO NÚMEROS INTEIROS EX: 1, 10, 100\033[m")
         continue
 
 # RESULTADO DA ESCOLHA DO VALOR DA TABUADA
+    if escolha_sinal == 1:
+        for i in range(1, 11):
+            print(f"{escolha_valor:2} X {i:2} = {i+escolha_valor:3}")
 
-    for i in range(1, 11):
-        print(f"{escolha:2} X {i:2} = {i*escolha:3}")
+    elif escolha_sinal == 2:
+        for i in range(1, 11):
+            print(f"{escolha_valor:2} X {i:2} = {i-escolha_valor:3}")
+
+    elif escolha_sinal == 3:
+        for i in range(1, 11):
+            print(f"{escolha_valor:2} X {i:2} = {i*escolha_valor:3}")
+
+    elif escolha_sinal == 4:
+        for i in range(1, 11):
+            print(f"{escolha_valor:2} X {i:2} = {i/escolha_valor:.2f}")
+
+    else:
+        print(
+            "ENTRADA INVÁLIDA!, TENTE APENAS NÚMEROS VÁLIDOS!, EX: 1, 2, 3, 4 = +, -, *, /")
     break
 
 # FIM DO PROJETO
