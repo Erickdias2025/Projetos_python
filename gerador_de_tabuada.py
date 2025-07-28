574eb6d96307bf813254d1607bb1a69365711d7f
Erick Dias <erickdias20255@gmail.com>
2025-07-28 18:14:42 -0300

    PRIMEIRA VERSÃO DO PROJETO

---
+++ gerador_de_tabuada.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        numero = int(input("ESCOLHA UM NÚMERO INTEIRO: "))
+        operador = input("ESCOLHA UM OPERADOR (+): ")
+    except ValueError:
+        print(
+            "\033[31mENTRÁDA INVALIDA, TENTE NOVAMENTE COM NÚMERO INTEIROS EX: 10, 50, 100\033[m")
+        continue
+
+    for i in range(1, 11):
+        if operador == "+":
+            print(f"{numero} + {i:2} = {numero+i}")
+    break
